5e5491ee2b904cec600c6cd844dac47ca07e2168
Jeremy Fields <j.c.delafields@gmail.com>
2022-04-21 18:05:12 +0000

    Made the ting perpetual

--- contracts/PerpetualLottery.sol
+++ contracts/PerpetualLottery.sol
@@ -3,20 +3,21 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ * @title PerpetualLottery
+ * @author delafields
+ * @notice This creates a raffle that runs in perpetuity
+ * @dev this is purely experimental
  */
-contract Lotto {
+contract PerpetualLottery {
 
     uint public pot = 0;
-    uint public ticketPrice;
-    uint public numTickets;
+    uint public ticketPrice = 100000000000000;
+    uint public houseFee    = 10000000000000;
+    uint public numTickets = 100;
     uint public ticketsInCirculation = 0;
     uint public currentTicketIndex = 0;
-    uint public maxTicketsPerWallet;
+    uint public maxTicketsPerWallet = numTickets % 10; // 10% of circ
     uint public startTime;
-    uint public houseFee = 10000000000000;
     uint raffleNumber = 0;
     bool raffleActive = false;
     address public owner;
@@ -25,78 +26,100 @@ contract Lotto {
         owner = msg.sender;
     }
 
-    // maps raffleNumber + adress -> number of tickets owned
+    // maps raffleNumber + address -> number of tickets owned
     mapping(uint => mapping(address => uint)) numTicketsHeld;
     // maps raffleNumber + ticketNumber -> owner address
     mapping(uint => mapping(uint => address)) ticketOwner;
 
-    event LottoStarted(uint ticketPrice, uint numTickets);
-    event LottoEnded(uint indexed winningTicketNumber, address indexed winningAddress);
+    event RaffleStarted(uint raffleNumber);
+    event RaffleEnded(uint indexed winningTicketNumber, address indexed winningAddress);
     event TicketsBought(address indexed buyer, uint numTicketsBought, uint numTicketsInCirculation, uint numTicketsLeft);
 
+    //@dev simple onlyOwner mod
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    function startLotto(uint _ticketPrice, uint _numTickets) external onlyOwner {
-        require(_numTickets >= 100 || _numTickets <= 1000, "Must be between 100 and 1000 tickets");
-        require(raffleActive == false, "There is already an active lottery");
-        require(_numTickets % 20 == 0, "Number of tickets must be a factor of 20");
+    //@notice this starts a new raffle
+    //@dev this must be called once upon deployment, after which it runs in perpetuity
+    function startRaffle() external onlyOwner {
+        require(raffleActive == false, "There is already an active raffle");
 
         raffleActive = true;
-        ticketPrice = _ticketPrice;
-        numTickets = _numTickets;
-        maxTicketsPerWallet = _numTickets / 20;
         startTime = block.timestamp;
         raffleNumber++;
 
-        emit LottoStarted(_ticketPrice, _numTickets);
+        emit RaffleStarted(raffleNumber);
     }
 
+    //@notice handles the ticket buying functionality
+    //@dev possible improvements could be made on amount handling
+    //@param _amount eth required to buy tickets
+    //@param _numToBuy number of tickets trying to be bought
     function buyTicket(uint _amount, uint _numToBuy) external payable {
         require(_numToBuy <= numTickets - ticketsInCirculation, "You're trying to buy more tickets than are available");
-        require(numTicketsHeld[raffleNumber][msg.sender] + _numToBuy <= maxTicketsPerWallet, "You can only own 5% of the tickets, max");
-        require(_amount >= (ticketPrice * _numToBuy) + houseFee, "Not paying enough for this number of tickets, + the house fee");
+        require(numTicketsHeld[raffleNumber][msg.sender] + _numToBuy <= maxTicketsPerWallet, "You can only own 10% of the tickets, max");
+        require(_amount >= (ticketPrice * _numToBuy) + houseFee, "Not paying enough for this number of tickets + the house fee");
+        require(msg.value == _amount);
 
-        ticketsInCirculation -= _numToBuy;
+        // log number of tickets in circulation
+        ticketsInCirculation += _numToBuy;
+
+        // log number of tickets owned
         numTicketsHeld[raffleNumber][msg.sender] += _numToBuy;
-
+        // map ticket number to its owner
         for (uint i = currentTicketIndex; i < currentTicketIndex + _numToBuy; i++) {
             ticketOwner[raffleNumber][i] = msg.sender;
             currentTicketIndex++;
         }
 
-        // TODO: make sure that this is actually transferring ether
-        // this probably needs msg.value or something
+        // add eth to the pot
         pot += _amount;
 
         emit TicketsBought(msg.sender, _numToBuy, ticketsInCirculation, numTickets - ticketsInCirculation);
+
+        // if every ticket has been bought, end raffle and kick off a new one
+        if (currentTicketIndex == numTickets) {
+            this.endRaffle();
+        }
     }
 
-    // blocktime: https://ethereum.stackexchange.com/questions/7853/is-the-block-timestamp-value-in-solidity-seconds-or-milliseconds
-    function endLotto() external onlyOwner {
-        // require block.timestamp > startTime
+    //@notice chooses a winning ticket, pays out owner, kicks off a new lotto
+    //@dev this is "blockchain random", could use an oracle
+    function endRaffle() external onlyOwner {
+        // lil baby check to make sure this isn't ddos'ed
+        require(block.timestamp >= startTime + 1 minutes, "This raffle just started!");
 
         uint winningTicketIndex = getRandomNumber();
         address winnerAddress = ticketOwner[raffleNumber][winningTicketIndex];
 
-        address(winnerAddress).transfer(pot);
+        // pay out the owner of the winning ticket
+        payable(winnerAddress).transfer(pot);
+        pot = 0;
 
+        // start a new raffle
         raffleActive = false;
-        ticketPrice = 0;
-        numTickets = 0;
-        maxTicketsPerWallet = 0;
+        ticketsInCirculation = 0;
+        currentTicketIndex = 0;
 
-        emit LottoEnded(winningTicketIndex, winnerAddress);
+        emit RaffleEnded(winningTicketIndex, winnerAddress);
+
+        this.startRaffle();
     }
 
-    /** chainlink VRF if I'm ever feelin' fancy:
+    //@dev keccak "random" number generator
+    function getRandomNumber() internal view returns (uint randomNumber) {
+        /** chainlink VRF if I'm ever feelin' fancy:
         * https://dapp-world.com/smartbook/how-to-get-random-number-in-chainlink-wgJy
         * https://stackoverflow.com/questions/48848948/how-to-generate-a-random-number-in-solidity
-    **/
-    function getRandomNumber() internal returns (uint randomNumber) {
-        uint kecHash = keccak256(abi.encodePacked(block.difficulty, now, currentTicketIndex));
+        **/
+
+        // grab a random ticket from those in circulation
+        uint kecHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, currentTicketIndex)));
         return kecHash % currentTicketIndex;
     }
+
+    // fallback triggered by ticket buying
+    receive() external payable { }
 }
