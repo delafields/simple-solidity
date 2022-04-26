bd3a319d551dfe1cc86f77f2e8ed339fc4e33170
Jeremy Fields <j.c.delafields@gmail.com>
2022-04-26 16:56:30 +0000

    upgradeable master contract p much done

--- contracts/Upgradeable/MasterContract.sol
+++ contracts/Upgradeable/MasterContract.sol
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
 // make this ownable
 contract MasterContract {
 
+    address public owner = msg.sender;
     address public currentVersionAddress;
     string  public currentReleaseNumber;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can call this");
+        _;
+    }
 
     event NewVersionReleased(
         address newVersionContractAddress, 
         string  newReleaseNumber
     );
 
-    // make ownable
     function setVersion(
         address _newVersionContractAddress,
         string  calldata _newReleaseNumber
-    ) external 
+    ) external onlyOwner
     {
         currentVersionAddress = _newVersionContractAddress;
         currentReleaseNumber  = _newReleaseNumber;
@@ -28,8 +32,16 @@ contract MasterContract {
         );
     }
 
-    // this should trigger the newest implementation
-    fallback() external {
+    function getCurrentVersion() 
+        public 
+        view
+        returns(address _currentVersionAddress, string memory _currentReleaseNumber)
+    {
+        return (currentVersionAddress, currentReleaseNumber);
+    }
+
+    // this should trigger the newest implementation's methods
+    fallback() external onlyOwner {
         (bool success, ) = currentVersionAddress.delegatecall(msg.data);
         require(success, "call to current version failed");
     }
