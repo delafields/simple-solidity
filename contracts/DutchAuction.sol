20ba7d6492568cc29c6f6648c94379a5a21980e1
Jeremy Fields <j.c.delafields@gmail.com>
2022-04-28 03:31:30 +0000

    almost done dutch auction (laptop is dying!!!)

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -2,9 +2,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-
-// make things ownable
 
 interface IERC721 {
     function transferFrom(
@@ -14,60 +11,85 @@ interface IERC721 {
     ) external;
 }
 
-contract DutchAuctioneer {
-    using Counters for Counters.Counter;
-    Counters.Counter private _auctionNumber;
+/**
+ * @title Dutch Auction
+ * @author delafields
+ * @notice This allows an nft owner to initiate a dutch auction for their nft
+ * @dev this is purely experimental
+ */
+contract DutchAuction {
 
-    constructor() {
-        // _auctionNumber is initialized to 1, since 0 == higher gas
-        _auctionNumber.increment();
-    }
-    
-    struct Auction {
-        uint auctionNumber;
-        uint hoursToRun;
-        uint endTime;
-        uint startingPrice;
-        uint reservePrice;
-        uint discountRate;
-        uint nftId;
-        address nftAddress;
-        bool active;
-    }
+    IERC721 public immutable nft;
 
-    mapping(uint => Auction) auctionRecords;
+    uint private constant DURATION = 7 days;
+    uint public immutable startingPrice;
+    uint public immutable reservePrice;
+    uint public immutable discountRate;
+    uint public immutable startTime;
+    uint public immutable endTime;
+    uint public immutable nftId;
+    address payable public immutable nftSeller;
 
-    event NewAuctionStarted(uint auctionNum, uint startingPrice, nftAddress);
-
-    function startAuction(
-        uint _duration,
+    /**
+    * @param _startingPrice High point price of NFT
+    * @param _reservePrice  Low point price of NFT
+    * @param _discountRate Rate at which the _startingPrice degrades over time
+    * @param _nftId Unique identifier of the NFT
+    * @param _nftAddress Address of the NFT
+    */
+    constructor(
         uint _startingPrice,
         uint _reservePrice,
         uint _discountRate,
         uint _nftId,
         address _nftAddress
-    ) external {
-        // requires
-        Auction newAuction = Auction({
-            auctionNumber: _auctionNumber.current(),
-            hoursToRun: _duration*60*60,
-            endTime: block.timestamp + duration*60*60,
-            startingPrice: _startingPrice,
-            reservePrice: _reservePrice,
-            discountRate: _discountRate,
-            nftId: _nftId,
-            nftAddress: _nftAddress,
-            active: true
-        });
+    )
+        payable
+    {
+        // Price must be degradeable via this simple formula
+        require(_startingPrice >= _discountRate * DURATION, "Price must be >= discount rate * duration (7 days)");
+        
+        startingPrice = _startingPrice;
+        reservePrice  = _reservePrice;
+        discountRate = _discountRate;
+        startTime = block.timestamp;
+        endTime = block.timestamp + DURATION;
+        nftId = _nftId;
+        nftSeller = payable(msg.sender);
 
-        auctionRecords[_auctionNumber.current()] = newAuction;
-        emit NewAuctionStarted(auctionNumber.current(), startingPrice, _nftAddress);
-        _auctionNumber.increment();
+        nft = IERC721(_nftAddress);
+
     }
 
-    function getPrice() public {}
+    /**
+    * @returns _currentPrice Price as a function of time since start
+    */
+    function getCurrentPrice() public view returns (uint _currentPrice) {
+        uint timeFromStart = block.timestamp - startTime;
+        // discount is a function of time & discount rate
+        uint dutchDiscount = discountRate * timeFromStart;
+        uint currentPrice = startingPrice - dutchDiscount;
 
-    function buy() {}
+        return currentPrice;
+    }
 
-    function endAuction() {}
+    /**
+
+    */
+    function buy() external payable {
+        require(block.timestamp < endTime, "Auction has expired");
+
+        uint currentPrice = getCurrentPrice();
+        require(currentPrice >= reservePrice, "The natural timed discount of this auction is below reserve.");
+        require(msg.value >= currentPrice, "The current price is > the amount of ETH sent");
+
+        nft.transferFrom(nftSeller, msg.sender, nftId);
+
+        uint refundAmount = msg.value - currentPrice;
+        if (refundAmount > 0) {
+            payable(msg.sender).transfer(refundAmount);
+        }
+
+        selfdestruct(nftSeller);
+    }
 }
